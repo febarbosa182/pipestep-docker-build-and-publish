f1eb2432b696f33dc674ac13b1fd1dfdf7c092c6
febarbosa182 <lybrbarbosa@gmail.com>
2021-09-27 14:41:52 -0300

    build: build with dind

--- src/br/com/dynamic/docker/BuildAndPublish.groovy
+++ src/br/com/dynamic/docker/BuildAndPublish.groovy
@@ -4,43 +4,55 @@ class PublishImage{
     def call (jenkins) {
 
         jenkins.podTemplate(
-            yaml: """
+          yaml: """
 apiVersion: v1
 kind: Pod
-metadata:
-  name: docker
-  labels:
-    app : docker
 spec:
-  containers:
-  - name: docker
-    image: docker:20.10.2
-    tty: true
-    imagePullPolicy: IfNotPresent
-    command:
-      - /bin/sh
-    volumeMounts:
-    - name: dockersock
-      mountPath: "/var/run/docker.sock"
   volumes:
-  - name: dockersock
-    hostPath:
-      path: /var/run/docker.sock
+  - name: docker-socket
+    emptyDir: {}
+  containers:
+  - name: aws-cli
+    image: amazon/aws-cli:2.2.27
+    ttyEnabled: true
+    command:
+    - sleep
+    args:
+    - 99d
+  - name: docker
+    image: docker:20.10.7
+    command:
+    - sleep
+    args:
+    - 99d
+    volumeMounts:
+    - name: docker-socket
+      mountPath: /var/run
+  - name: docker-daemon
+    image: docker:20.10.7-dind
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: docker-socket
+      mountPath: /var/run
             """,
-            yamlMergeStrategy: jenkins.merge(),
-            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
-                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
-                readOnly: false
-            )
-        )
+          yamlMergeStrategy: jenkins.merge(),
+          workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+              claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+              readOnly: false
+          )
+        ) 
         {
             jenkins.node(jenkins.POD_LABEL){
-                jenkins.container('docker'){
-                    jenkins.echo "Build and Publish Docker image Step"
-
-                    jenkins.docker.build("\${DOCKER_IMAGE}:\${APP_VERSION}.\${GIT_COMMIT}","--network=host .")
-                    // jenkins.docker.image("\${DOCKER_IMAGE}:\${APP_VERSION}.\${GIT_COMMIT}").push()
+              jenkins.container('docker'){
+                jenkins.echo "Build and Publish Docker image Step"
+                jenkins.docker.withRegistry("https://public.ecr.aws/i8c9l4d5"){
+                  // jenkins.docker.build("\${DOCKER_IMAGE}:\${APP_VERSION}.\${GIT_COMMIT}")
+                  jenkins.sh script:"DOCKER_BUILDKIT=1 docker build --progress plain -t \${DOCKER_IMAGE}:\${APP_VERSION}.\${GIT_COMMIT}.\${BUILD_NUMBER} .",
+                              label: "Building docker"
+                  jenkins.docker.image("\${DOCKER_IMAGE}:\${APP_VERSION}.\${GIT_COMMIT}.\${BUILD_NUMBER}").push()
                 }
+              }
             }
         }
     }
